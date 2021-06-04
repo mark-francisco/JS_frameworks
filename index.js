b99c7427b304a3029bbd8f706c68011b1be54f34
Mark Francisco <mfrancisco581@gmail.com>
2021-06-03 20:45:04 -0500

    solve the  cors issue that was not a real issue by using the correct api urls

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 /* global axios */
-// const axios = require('axios').default;
+
+function Language(name, watchers, stars, forks) {
+  this.name = name;
+  this.watchers = watchers;
+  this.stars = stars;
+  this.forks = forks;
+}
+// Language.prototype.methodNameHere = function () {
+//   return "this is where you would define the fn's behavior";
+// };
+let vue = new Language("Vue", 1, 2, 3);
+
+// let angular = {};
+// let ember = {};
+// let svelte = {};
+// let react = {};
 
 
-axios.get("https://github.com/vuejs/vue")
+axios.get("https://api.github.com/repos/vuejs/vue")
   .then(function (response) {
-    console.log(response);
+    console.log(response.data);
   })
   .catch(function (error) {
     console.log(error);
