ac101c2c2ed4e28a17fa505b769789fd12d802fd
Mark Francisco <mfrancisco581@gmail.com>
2021-06-08 16:55:10 -0500

    add multiple bars per language to graph

--- index.js
+++ index.js
@@ -75,6 +75,25 @@ setTimeout(function makeChart() {
           ],
           borderWidth: 1,
         },
+        {
+          label: "Watchers",
+          data: [languages[0]["watchers"],languages[1]["watchers"],languages[2]["watchers"],languages[3]["watchers"],languages[4]["watchers"],],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+          ],
+          borderWidth: 1,
+        },
       ],
     },
     options: {
